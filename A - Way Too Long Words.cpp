15d90f20d02b1606ccff533f8c167abaf86b2e98
JatinYadav6618 <83481545+JatinYadav6618@users.noreply.github.com>
2021-06-17 10:32:52 +0530

    Add files via upload

---
+++ A - Way Too Long Words.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;cin>>n;
+while(n--){
+    string s;
+    cin>>s;
+    int size=s.length();
+    if(size>10){
+        cout<<s[0]<<size-2<<s[size-1]<<endl;
+    }
+    else{
+        
+        cout<<s<<endl;
+       
+    }
+    
+}
+return 0;
+
+}
